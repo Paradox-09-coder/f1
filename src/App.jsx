d91ffa52cbe8bc25caebbdeaaf88119c90ca3b7f
Paradox-09-coder <qayumovabdu2009@gmail.com>
2026-05-13 16:22:56 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import Home from './pages/Home';
 import Drivers from './pages/Drivers';
 import News from './pages/News';
 import Results from './pages/Results';
-import Schedule from './pages/Schedule';
 import Teams from './pages/Teams';
 
 // Not Found Page
@@ -15,6 +14,7 @@ import NotFoundPage from './pages/NotFoundPage';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Trassa from './pages/Trassa';
 
 function App() {
   return (
@@ -26,7 +26,7 @@ function App() {
         <Route path='/drivers' element={<Drivers />} />
         <Route path='/news' element={<News />} />
         <Route path='/results' element={<Results />} />
-        <Route path='/schedule' element={<Schedule />} />
+        <Route path='/schedule' element={<Trassa />} />
         <Route path='/teams' element={<Teams />} />
         
         <Route path='*' element={<NotFoundPage />} />
---
+++ src/pages/Trassa.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Trassa() {
+  return (
+    <div>Trassa</div>
+  )
+}
