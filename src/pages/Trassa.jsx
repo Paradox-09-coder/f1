714c4eebba970a785fc19f85fb78e22240ad03df
Odilbek <odilbeknabijonib19.com>
2026-05-15 17:31:57 +0500

    trassa.css qoshildi
    ozgartirildi

--- src/pages/Trassa.jsx
+++ src/pages/Trassa.jsx
@@ -2,6 +2,24 @@ import React from 'react'
 
 export default function Trassa() {
   return (
-    <div>Trassa</div>
+    <div className='Trassa max-w-[1640px] ' >
+
+
+      <div className="header">
+        <select >
+          <option className=''>2026</option>
+          <option>2025</option>
+          <option>2024</option>
+          <option>2023</option>
+          <option>2022</option>
+          <option>2021</option>
+          <option>2020</option>
+        </select>
+      </div>
+
+
+
+
+    </div>
   )
 }
