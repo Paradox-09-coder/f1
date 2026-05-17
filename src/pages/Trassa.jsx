86fe208b6328b2994f077e038b2f5c6a4ab388e9
Odilbek <odilbeknabijonib19.com>
2026-05-17 15:09:09 +0500

    y

--- src/pages/Trassa.jsx
+++ src/pages/Trassa.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Trassa.css';
-import cardlar from './Api';
+import { cardlar } from './Api';
+import { cardlar1 } from './Api';
+import { cardlar2 } from './Api';
 
 
 const Trassa = () => {
@@ -22,16 +24,16 @@ const Trassa = () => {
       <div className="container h-[750px] mt-[300px] bg-[#000]">
 
         {/* 1 */}
- <div class="select-container" tabindex="0">
-  <select className='web bg-[#000]' >
+        <div class="select-container" tabindex="0">
+          <select className='web bg-[#000]' >
 
-  <option class="select-text">2026</option>
-  <option class="select-text">2026</option>
-  <option class="select-text">2026</option>
-  <option class="select-text">2026</option>
-  </select>
+            <option class="select-text">2026</option>
+            <option class="select-text">2026</option>
+            <option class="select-text">2026</option>
+            <option class="select-text">2026</option>
+          </select>
 
-</div>
+        </div>
         {/* 2 */}
         <div className="orta flex  justify-between    bg-[#000] ">
           <h1 className='a1  text-[#fff] text-[40px] font-inter mt-[30px]'> 2026 FIA Formula One World Championship™ Race
@@ -44,8 +46,8 @@ const Trassa = () => {
 
 
 
-           
-           {/* 3 */}
+
+        {/* 3 */}
 
         <div className="past flex justify-center gap-[15px] flex-wrap mt-[50px]">
           <div className='w1'> <h1 className='a2'>Previous</h1>  </div>
@@ -61,34 +63,101 @@ const Trassa = () => {
 
 
 
-   <div className="main  ">
+      <div className="main  ">
 
-    <div className="api  flex">
-      {
+        <div className="api  flex justify-center gap-[20px] flex-wrap">
+          {
 
-        cardlar.map((value)=>{
-          return(
-            <div key={value.id} className="v flex items-center w-[520px] h-[230px]  bg-[#000000]">
-              <div className="s">
+            cardlar.map((value) => {
+              return (
+                <div key={value.id} className="v flex items-center w-[520px] h-[230px]  bg-[#000000]  rounded-[8px] ">
+                  <div className="s">
 
-              <h3 className='text-[#AAAAAA] font-inter indent-[20px] text-[14px] t'>{value.p}</h3>
-            <h1 className='text-[#fff] text-[24px] font-bold  indent-[10px] flex  t'>
-                <img className='w-[30px] h-[30px]  rounded-[50%] border-3 border-[#fff] n' src={value.nomi} alt="" />Bahrain
-              </h1>  
-              <p className='text-[#AAA]  text-[14px] indent-[20px] t'>{value.malumot}</p>
-              <h2 className='text-[#fff] font-bold indent-[20px] text-[16px] z '>{value.kun}</h2>
-              </div>
+                    <h3 className='text-[#AAAAAA] font-inter indent-[20px] text-[14Rpx] t'>{value.p}   </h3>
+                    <h1 className='text-[#fff] text-[24px] font-bold  indent-[10px] flex  t'>
+                      <img className='w-[30px] h-[30px]  rounded-[50%] border-3 border-[#fff] n' src={value.nomi} alt="" />Bahrain
+                    </h1>
+                    <p className='text-[#AAA]  text-[14px] indent-[20px] t'>{value.malumot}</p>
+                    <h2 className='text-[#fff] font-bold indent-[20px] text-[16px] z '>{value.kun}</h2>
+                  </div>
 
-              <div className="f float-right  ">
-              <img  className='right w-[148px] h-[198px] ' src={value.img} alt="" />
-              </div>
-            </div>
-          )
-        })
-      }
-    </div>
+                  <div className="f float-right  ">
+                    <img className='right w-[148px] h-[198px] ' src={value.img} alt="" />
+                  </div>
+                </div>
+              )
+            })
 
-   </div>
+          }
+
+
+          {
+
+            cardlar1.map((value) => {
+              return (
+
+                <div key={value.id} className="v    w-[520px] h-[230px]  bg-[#000000]  rounded-[8px] ">
+
+                    <div className="card2  flex   justify-between">
+
+                  <h3 className='text-[#AAAAAA] font-inter indent-[20px] text-[14px] avstraliya'>{value.p}   </h3>
+                  <span className=' ong w-[140px]  h-[40px]  bg-[#15151E] '><i class="fa-solid fa-flag-checkered"></i>   {value.center}  </span>
+                    </div>
+                  <h1 className='text-[#fff] text-[24px] font-bold  indent-[10px] flex  t'>
+                    <img className='w-[30px] h-[30px] rounded-[50%]  border-3 border-[#fff] n' src={value.bayroq1} alt="" />
+                    {value.nomi}</h1>
+                  <p className='text-[#AAA]  text-[14px] indent-[20px] t'>{value.malumot}</p>
+
+                  <div className="cardlar1  flex  ">
+
+
+                  <img className='rasm1 w-[180px] h-[48px] ' src={value.rasm1} alt="" />
+                  <img className=' w-[147px] h-[48px] ' src={value.rasm2} alt="" />
+                  <img className=' w-[153px] h-[48px] ' src={value.rasm3} alt="" />
+                  </div>
+
+                </div>
+
+              )
+            })
+          }
+        
+        {
+         
+         
+         cardlar2.map((value)=>{
+           return(
+             <div key={value.id} className="v    w-[520px] h-[230px]  bg-[#000000]  rounded-[8px] ">
+
+                            <div className="s  flex   justify-between">
+
+                  <h3 className='text-[#AAAAAA] font-inter indent-[20px] text-[14px] avstraliya'>{value.p}   </h3>
+                  <span className=' ong w-[140px]  h-[40px]  bg-[#fff] text-[black] '>   NEXT RACE  </span>
+                    </div>
+                  <h1 className='text-[#fff] text-[24px] font-bold  indent-[10px] flex  t'>
+                    <img className='w-[30px] h-[30px] rounded-[50%]  border-3 border-[#fff] n' src={value.bayroq1} alt="" />
+                    {value.nomi}</h1>
+                  <p className='text-[#AAA]  text-[14px] indent-[20px] t'>{value.malumot}</p>
+
+                  <div className="cardlar1  flex  ">
+
+
+                 
+                  </div>
+
+              
+             </div>
+           )
+         })
+        }
+          
+        </div>
+
+
+
+
+
+      </div>
 
 
     </div>
